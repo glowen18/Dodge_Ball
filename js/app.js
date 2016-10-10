cab8fa05330b7bcdfd61b2fa7a1851e4e7cb79e2
Gaye Lowenstein <glowen@hotmail.com>
2016-10-10 12:55:20 -0700

    added 9 circles onto gameboard canvas

--- js/app.js
+++ js/app.js
@@ -1,12 +1,108 @@
-$(document).ready(function() {
-  var canvas = document.getElementById("myCanvas");
-  var ctx = canvas.getContext("2d");
+// *********** Draw the playground box **************
+  var canvas = document.getElementById('myCanvas');
+  var ctx = canvas.getContext('2d');
+
 
   ctx.beginPath();
-  ctx.rect(100, 100, 400, 400);
-  ctx.strokeStyle = "rgb(0, 0, 255)";
-  ctx.stroke();
+  ctx.arc(200, 200, 20, 0, 2*Math.PI, true);
   ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(300, 200, 20, 0, 2*Math.PI, true);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(400, 200, 20, 0, 2*Math.PI, true);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(200, 300, 20, 0, 2*Math.PI, true);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(300, 300, 20, 0, 2*Math.PI, true);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(400, 300, 20, 0, 2*Math.PI, true);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(200, 400, 20, 0, 2*Math.PI, true);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(300, 400, 20, 0, 2*Math.PI, true);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(400, 400, 20, 0, 2*Math.PI, true);
+  ctx.closePath();
+  ctx.stroke();
 
 
-})
+
+  // circle.moveTo(200,200);
+  // circle.arc(100, 35, 25, 0, 2*Math.PI);
+  // ctx.stroke(circle);
+
+
+
+
+
+
+  // *********** Draw a BALL **************
+  // ctx.beginPath();
+  // ctx.arc(240,160,10,0, Math.PI*2, false);
+  // ctx.fillStyle = "purple";
+  // ctx.fill();
+  // ctx.closePath();
+
+  // *********** Function to MOVE the BALL **************
+var x = myCanvas.width-300;
+var y = myCanvas.height-100;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
+
+// ******** Function to DRAW the BALL
+  function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "purple";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+// ******** Function to remove the trail of the ball
+  // function draw() {
+  //   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //   drawBall();
+  // }
+
+// ******** Move the Ball
+  // if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+  //   dx = -dx;
+  // }
+  //
+  // if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+  //   dy = -dy;
+  // } else if (y + dy > canvas.height-ballRadius) {
+  //   alert("Game Over! :/(")
+  //   document.location.reload();
+  // }
+  //   x += dx;
+  //   y += dy;
+  // }
+  //
+  // setInterval(draw, 10);
+
+// })
