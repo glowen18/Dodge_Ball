e1c7cbb281df12b8b67d1144ec678db948466a9e
Gaye Lowenstein <glowen@hotmail.com>
2016-10-10 10:26:24 -0700

    added canvas box to my game

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("This is working... yay!")
+$(document).ready(function() {
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+
+  ctx.beginPath();
+  ctx.rect(100, 100, 400, 400);
+  ctx.strokeStyle = "rgb(0, 0, 255)";
+  ctx.stroke();
+  ctx.closePath();
+
+
+})
