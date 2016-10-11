41eabf4ae9c131a6b611146827fb84c204506f5a
Gaye Lowenstein <glowen@hotmail.com>
2016-10-11 16:50:13 -0700

    working on moving player

--- js/app.js
+++ js/app.js
@@ -16,8 +16,6 @@ function startGame(){
   $('#start').off();
 };
 
-
-
 //create the circles on the canvas
 function drawGrid () {
   ctx.beginPath();
@@ -89,13 +87,41 @@ function drawGuy() {
   ctx.drawImage(img, 280, 280, 50, 50)
 }
 
+  $(document).ready(function(){
+    $("#myGuy").focus();
+    ("#myGuy").on('keydown', function(event){
+      switch (event.which){
+        case 37:
+          $('#myGuy').stop().animate({
+            left: '-=10'
+          }); //left arrow key
+          break;
+        case 38:
+          $('#myGuy').stop().animate({
+            top: '-=10'
+          }); //up arrow key
+          break;
+        case 39:
+          $('#myGuy').stop().animate({
+            left: '+=10'
+          })  // right arrow key
+          break;
+        case 40:
+          $('#myGuy').stop().animate({
+            top: '+=10'
+          }); //bottom arrow key
+          break;
+    }
+  });
+});
+
 //******** Redraw the moving ball ************//
 function reDraw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
     createBall();
     drawGrid();
     drawGuy();
-    
+
 
 // ******** Move the Ball *************//
   if(x + dx > canvas.width || x + dx < 0) {
